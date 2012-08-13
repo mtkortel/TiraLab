d2c8f486064e03bd41556c34800315aaab9cd634
Marko <me@you.com>
2012-08-13 23:11:39 +0300

    a

---
+++ TiraLab/src/tiralab/huffman/CharList.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralab.huffman;
+
+/**
+ *
+ * @author marko
+ */
+public class CharList{
+    private final int def_size = 100;
+    private char[] lista;
+    private int koko;
+    /**
+     * Oletuskonstruktori joka alustaa listan
+     * 
+     */
+    public CharList(){
+        lista = new char[def_size];
+        koko = 0;
+    }
+    
+    public void add(char new_char) {
+        if (koko == lista.length){
+            kasvataKoko();
+        }
+        lista[koko] = new_char;
+        koko++;
+    }
+
+    public char get(int i) {
+        if (i < lista.length){
+            return lista[i];
+        } else {
+            return '0';
+        }
+    }
+
+    private void kasvataKoko() {
+        char[] tmp = new char[lista.length*2];
+        for (int i=0; i < lista.length; i++){
+            tmp[i] = lista[i];
+        }
+        lista = tmp;
+    }
+    
+}
---
+++ TiraLab/src/tiralab/huffman/IOmaLista.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralab.huffman;
+
+/**
+ * Abstract luokka listarakenteesta.
+ *
+ * @author marko
+ */
+public interface IOmaLista {
+    public abstract void add(Object obj);
+    public abstract Object get(int i);
+}
